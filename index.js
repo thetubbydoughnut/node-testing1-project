65c35c985abcce306f69ec0cc5ee1125da03b88b
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-10-30 12:24:43 -0500

    wip

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -135,7 +136,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if (distance <= milesCanDrive) {
+      this.odometer = this.odometer + distance 
+      this.tank = this.tank - (distance / this.mpg)
+    } else {
+      this.odometer = this.odometer + milesCanDrive
+      this.tank = 0
+    }
+    return this.odometer
   }
 
   /**
@@ -150,7 +159,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
